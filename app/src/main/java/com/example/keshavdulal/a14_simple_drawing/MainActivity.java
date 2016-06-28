2b1026aebfe38c980ede13f86244b3c7904063a4
imaslarab <baralsami@gmail.com>
2016-06-28 10:13:24 +0545

    first

--- app/src/main/java/com/example/keshavdulal/a14_simple_drawing/MainActivity.java
+++ app/src/main/java/com/example/keshavdulal/a14_simple_drawing/MainActivity.java
@@ -27,11 +27,57 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onDraw(Canvas canvas) {
             //super.onDraw(canvas);
-            canvas.drawColor(Color.BLACK);          //BACKGROUND
-            Paint circlePaintObj = new Paint();     //Paint Object
-            circlePaintObj.setColor(Color.RED);
-            canvas.drawCircle(canvas.getWidth() / 2, canvas.getHeight() / 2, canvas.getWidth() / 3, circlePaintObj);
+            //canvas.drawColor(Color.BLACK);          //BACKGROUND
+            Paint graphBoundaryObj = new Paint();     //Paint Object
+            graphBoundaryObj.setColor(Color.RED);
+            graphBoundaryObj.setStrokeWidth(7);
+
+            //canvas.drawCircle(canvas.getWidth() / 2, canvas.getHeight() / 2, canvas.getWidth() / 3, graphBoundaryObj);
             //x,y,radius,paintobj
+
+            float OX = 0;
+            float OY= 900;
+
+            float AX = 1450;
+            float AY = 900;
+
+//            float BX = 0;
+//            float BY = 150;
+
+            float parallel_dist = 800;
+            float graph_height =1;
+            //OA
+            canvas.drawLine(OX,OY,AX,AY,graphBoundaryObj);
+            //OB
+            //canvas.drawLine(OX,OY,BX,BY,graphBoundaryObj);
+
+            //Line Parallel to OA
+            canvas.drawLine(OX,OY-parallel_dist,AX,AY-parallel_dist,graphBoundaryObj);
+            //Midline - Divider
+            graphBoundaryObj.setStrokeWidth(1);
+            canvas.drawLine(OX,OY-(parallel_dist/2),AX,AY-(parallel_dist/2),graphBoundaryObj);
+
+            //Vertical graphical line
+            Paint graphLinesObj = new Paint();        //GLO graph-lines-object
+            graphLinesObj.setColor(Color.BLACK);
+            //graphLinesObj.setStrokeWidth(1);
+
+//            canvas.drawLine(OX,OY,OX,OY-graph_height,graphLinesObj);
+           // int dir = 1;                //1-up 2-down
+
+            //Ups & downs Graph
+            int angle=0;
+            for(OX = 0; OX<1450;OX+=10){
+
+                graph_height = (float) (200*Math.sin(angle*3.141/180));
+                canvas.drawLine(OX,OY-(parallel_dist/2),OX,OY-graph_height-(parallel_dist/2),graphLinesObj);
+//
+                angle=angle+3;
+//
+
+            }
+
+
         }
     }
 }
