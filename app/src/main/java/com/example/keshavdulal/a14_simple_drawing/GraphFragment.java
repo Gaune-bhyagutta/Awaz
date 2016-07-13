da99b4fff884267801510bf39d6998950f28e377
Keshavdulal <Keshav.dulal@gmail.com>
2016-07-13 11:23:39 +0545

    Fix Playback Error/ V1.0 Ready :)

--- app/src/main/java/com/example/keshavdulal/a14_simple_drawing/GraphFragment.java
+++ app/src/main/java/com/example/keshavdulal/a14_simple_drawing/GraphFragment.java
@@ -11,29 +11,27 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
-/**
- * Created by imas on 6/28/16.
- */
-public class GraphFragment extends Fragment{
+import java.io.IOException;
 
-    public static  float graph_height;
+public class GraphFragment extends Fragment {
+
+    public static float graph_height;
     static short[] recordAudioData = null;
     static short[] playAudioData = null;
     static MainActivity mainActivity = new MainActivity();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View view = inflater.inflate(R.layout.graph_fragment, container, false);
-
         return view;
     }
 
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState){
-        LinearLayout linearLayout = (LinearLayout)view.findViewById(R.id.rect);
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.rect);
         linearLayout.addView(new myGraphView(getActivity()));
-       // Log.d("VIVZ", "Linear Layout - "+linearLayout.getHeight());
+        // Log.d("VIVZ", "Linear Layout - "+linearLayout.getHeight());
     }
 
     public static class myGraphView extends View {
@@ -53,43 +51,47 @@ public class GraphFragment extends Fragment{
             graphBoundaryObj.setStrokeWidth(7);
 
             float X1 = 0;
-            float Y1 = canvas.getHeight()/2;
+            float Y1 = canvas.getHeight() / 2;
             float AX = canvas.getWidth();
-            float AY = canvas.getHeight()/2;
-            float X2,Y2;
+            float AY = canvas.getHeight() / 2;
+            float X2, Y2;
             drawMeshLines(canvas);
 
             //Midline - Divider
             graphBoundaryObj.setStrokeWidth(2);
-            canvas.drawLine(X1,Y1,AX,AY,graphBoundaryObj);
+            canvas.drawLine(X1, Y1, AX, AY, graphBoundaryObj);
             //Vertical graphical line
             Paint graphLinesObj = new Paint();        //GLO graph-lines-object
             graphLinesObj.setColor(Color.parseColor("#880000"));
             graphLinesObj.setStrokeWidth(5);
 
-            if (recordAudioData==null){
-                int length=mainActivity.getRecordBufferSize();
+            if (recordAudioData == null) {
+                int length = mainActivity.getRecordBufferSize();
                 recordAudioData = new short[length];
             }
-            if (playAudioData==null){
-                int length=mainActivity.getPlayBufferSize();
+            if (playAudioData == null) {
+                int length = mainActivity.getPlayBufferSize();
+                length = length/4;
                 playAudioData = new short[length];
             }
-            int recordBuffIndex = (recordAudioData.length/2-canvas.getWidth())/2;
-            int playBuffIndex = (playAudioData.length/2-canvas.getWidth())/2;
+            int recordBuffIndex = (recordAudioData.length / 2 - canvas.getWidth()) / 2;
+            int playBuffIndex = (playAudioData.length / 2 - canvas.getWidth()) / 2;
 
-            if (MainActivity.playState()==1){
-                for(X1 = 0; X1<=canvas.getWidth();X1++){
-                    graph_height = ((float) playAudioData[playBuffIndex])/50;
-                    X2=X1;
-                    Y2=Y1-graph_height;
-                    canvas.drawLine(X1,Y1,X2,Y2,graphLinesObj);
+            if (MainActivity.playState() == 1) {
+                for (X1 = 0; X1 <= canvas.getWidth(); X1++) {
+                    try {
+                        graph_height = ((float) playAudioData[playBuffIndex]) / 50;
+                    }
+                    catch (NullPointerException e){}
+                    X2 = X1;
+                    Y2 = Y1 - graph_height;
+                    canvas.drawLine(X1, Y1, X2, Y2, graphLinesObj);
                     playBuffIndex++;
                 }
                 postInvalidateDelayed(1);
 //                invalidate();
-            }else {
-                for (X1 = 0; X1 <=canvas.getWidth(); X1++) {
+            } else {
+                for (X1 = 0; X1 <= canvas.getWidth(); X1++) {
                     graph_height = ((float) recordAudioData[recordBuffIndex]) / 50;
                     X2 = X1;
                     Y2 = Y1 - graph_height;
@@ -101,48 +103,52 @@ public class GraphFragment extends Fragment{
             }
         }
 
-        public void drawMeshLines(Canvas canvas){
+        public void drawMeshLines(Canvas canvas) {
             //Mesh Lines
             int cgh = canvas.getHeight();
             int cgw = canvas.getWidth();
-            int cgh2 = cgh/2;
-            int cgw2 = cgw/2;
-            int meshDim = cgh2/20;
+            int cgh2 = cgh / 2;
+            int cgw2 = cgw / 2;
+            int meshDim = cgh2 / 20;
             int i;
             Paint meshObj = new Paint();
             meshObj.setColor(Color.parseColor("#808080"));
             meshObj.setStrokeWidth(1);
 
             //Horizontal Lines - Top Segment
-            for (i = cgh2; i>=0; i-=meshDim){
-                canvas.drawLine(0, i, cgw, i,meshObj);
+            for (i = cgh2; i >= 0; i -= meshDim) {
+                canvas.drawLine(0, i, cgw, i, meshObj);
             }
             //Horizontal Lines - Bottom Segment
-            for (i = cgh2; i<=cgh; i+=meshDim){
-                canvas.drawLine(0, i, cgw, i,meshObj);
+            for (i = cgh2; i <= cgh; i += meshDim) {
+                canvas.drawLine(0, i, cgw, i, meshObj);
             }
 
             //Vertical Lines - Left Segment
-            for (i = cgw2; i>=0; i-=meshDim){
-                canvas.drawLine(i,0, i,cgh,meshObj);
+            for (i = cgw2; i >= 0; i -= meshDim) {
+                canvas.drawLine(i, 0, i, cgh, meshObj);
             }
             //Vertical Lines - Right Segment
-            for (i = cgw2; i<= cgw; i+=meshDim){
-                canvas.drawLine(i,0, i,cgh,meshObj);
+            for (i = cgw2; i <= cgw; i += meshDim) {
+                canvas.drawLine(i, 0, i, cgh, meshObj);
             }
         }
     }
+
     //updating audiodata from mainactivity
-    public void updateRecordGraph(short[] data){
+    public void updateRecordGraph(short[] data) {
         recordAudioData = data;
     }
-    public void setRecordBufferSize(int size){
+
+    public void setRecordBufferSize(int size) {
         recordAudioData = new short[size];
     }
-    public void updatePlayGraph(short[] data){
+
+    public void updatePlayGraph(short[] data) {
         playAudioData = data;
     }
-    public void setPlayBufferSize(int size){
+
+    public void setPlayBufferSize(int size) {
         playAudioData = new short[size];
     }
 }
