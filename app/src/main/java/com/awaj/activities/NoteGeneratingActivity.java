6cf05523326f1fd7d82fef6f1fadd346a908efb0
Keshavdulal <Keshav.dulal@gmail.com>
2016-08-20 13:08:37 +0545

    Note generator UI Button state change added

--- app/src/main/java/com/awaj/activities/NoteGeneratingActivity.java
+++ app/src/main/java/com/awaj/activities/NoteGeneratingActivity.java
@@ -5,14 +5,19 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.awaj.R;
 
 /**
  * Created by keshavdulal on 08/08/16.
  */
-public class NoteGeneratingActivity extends AppCompatActivity{
+public class NoteGeneratingActivity extends AppCompatActivity {
     private Toolbar toolbarObj;
+    private Button monitorButton;
+    private boolean monitorButtonState = false;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,13 +30,31 @@ public class NoteGeneratingActivity extends AppCompatActivity{
         /**TODO:fix the back button*/
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        monitorButton = (Button) findViewById(R.id.monitorButton);
 
+        monitorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /**Monitoring*/
+                if (monitorButtonState == true) {
+                    monitorButtonState = false;
+                    monitorButton.setText("Start Monitoring");
+                }
+                /**Not Monitoring*/
+                else {
+                    monitorButtonState = true;
+                    monitorButton.setText("Stop Monitoring");
+                }
+            }
+        });
     }
 
-    /**Adding Functionality to the Back Arrow button to go back to previous Activity*/
+    /**
+     * Adding Functionality to the Back Arrow button to go back to previous Activity
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case android.R.id.home:
                 this.finish();
                 return true;
